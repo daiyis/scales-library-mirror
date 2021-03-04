42bf070d632d4802f96e5c199bc1f085a807bbd8
Leo Dai <leo@jobadder.com>
2021-03-04 17:00:44 +1100

    add scales theme file

--- projects/scales-library/src/public-api.ts
+++ projects/scales-library/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/scales-library.service';
 export * from './lib/scales-library.component';
 export * from './lib/scales-library.module';
+export * from './lib/search/search.component';
+
+export * from './lib/nz-zorro.module';
