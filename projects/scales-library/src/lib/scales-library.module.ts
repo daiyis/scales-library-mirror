6baef27b697b69f081ca627836b23834e478b30d
Leo Dai <leo@jobadder.com>
2021-03-08 12:32:04 +1100

    remove the cli auto-generated components and services

--- projects/scales-library/src/lib/scales-library.module.ts
+++ projects/scales-library/src/lib/scales-library.module.ts
@@ -1,14 +1,13 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { ScalesLibraryComponent } from './scales-library.component';
 import { NgZorroAntdModule } from './nz-zorro.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [ScalesLibraryComponent, SearchComponent],
+  declarations: [SearchComponent],
   imports: [NgZorroAntdModule, BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
-  exports: [ScalesLibraryComponent, SearchComponent, NgZorroAntdModule],
+  exports: [SearchComponent, NgZorroAntdModule],
 })
 export class ScalesLibraryModule {}
--- projects/scales-library/src/public-api.ts
+++ projects/scales-library/src/public-api.ts
@@ -2,8 +2,6 @@
  * Public API Surface of scales-library
  */
 
-export * from './lib/scales-library.service';
-export * from './lib/scales-library.component';
 export * from './lib/scales-library.module';
 export * from './lib/search/search.component';
 
