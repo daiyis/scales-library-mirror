ee368248da6487dbfdb56f01dee6aab8baec0950
Leo Dai <leo@jobadder.com>
2021-03-15 12:57:53 +1100

    add tag and select component

--- projects/scales-sandbox/src/app/app.module.ts
+++ projects/scales-sandbox/src/app/app.module.ts
@@ -7,17 +7,26 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SelectDemoComponent } from './select-demo/select-demo.component';
+import { InputDemoComponent } from './input-demo/input-demo.component';
 
 // import {ScalesLibraryModule} from 'dist/scales-library/scales-library';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule, ScalesLibraryModule],
+  declarations: [AppComponent, SelectDemoComponent, InputDemoComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ScalesLibraryModule,
+  ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
---
+++ projects/scales-sandbox/src/app/select-demo/select-demo.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-demo',
+  templateUrl: './select-demo.component.html',
+  styleUrls: ['./select-demo.component.scss'],
+})
+export class SelectDemoComponent implements OnInit {
+  listOfOption: string[] = [];
+  listOfSelectedValue = ['a10', 'c12'];
+
+  ngOnInit(): void {
+    const children: string[] = [];
+    for (let i = 10; i < 36; i++) {
+      children.push(`${i.toString(36)}${i}`);
+    }
+    this.listOfOption = children;
+  }
+}
