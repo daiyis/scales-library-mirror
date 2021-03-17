868b21c893947e7df9369a5ba93c209837270fb2
Leo Dai <leo@jobadder.com>
2021-03-17 15:01:19 +1100

    add datepicker, timepicker, and select

--- projects/scales-sandbox/src/app/select-demo/select-demo.component.ts
+++ projects/scales-sandbox/src/app/select-demo/select-demo.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
 @Component({
   selector: 'app-select-demo',
   templateUrl: './select-demo.component.html',
@@ -8,6 +15,29 @@ import { Component, OnInit } from '@angular/core';
 export class SelectDemoComponent implements OnInit {
   listOfOption: string[] = [];
   listOfSelectedValue = ['a10', 'c12'];
+  date = null;
+  time: Date | null = null;
+  defaultOpenValue = new Date(0, 0, 0, 0, 0, 0);
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ];
 
   ngOnInit(): void {
     const children: string[] = [];
