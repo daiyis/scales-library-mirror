417950e4a9ace12fc64a8c28240e5cdaf3b950d0
Leo Dai <leo@jobadder.com>
2021-03-19 09:54:45 +1100

    add scales-site application

---
+++ projects/scales-site/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { ScalesLibraryModule } from './../../../scales-library/src/lib/scales-library.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ScalesLibraryModule,
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
