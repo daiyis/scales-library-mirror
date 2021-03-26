d45081289fefe8074d1c1c5de0ce7e18f489fc57
Leo Dai <leo@jobadder.com>
2021-03-26 11:54:02 +1100

    add prismjs package

--- projects/scales-site/src/app/component-template/component-template.component.ts
+++ projects/scales-site/src/app/component-template/component-template.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HighlightService } from '../shared/highlight/highlight.service';
 
 @Component({
   selector: 'app-component-template',
   templateUrl: './component-template.component.html',
-  styleUrls: ['./component-template.component.scss']
+  styleUrls: ['./component-template.component.scss'],
 })
 export class ComponentTemplateComponent implements OnInit {
+  private highlighted: boolean = false;
+  constructor(private highlightService: HighlightService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  // ngAfterViewChecked() {
+  //   if (!this.highlighted) {
+  //     // this.highlightService.highlightAll();
+  //     this.highlighted = true;
+  //   }
+  // }
 }
--- projects/scales-site/src/app/shared/highlight/highlight.module.ts
+++ projects/scales-site/src/app/shared/highlight/highlight.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NzHighlightComponent } from './highlight.component';
+import { HighlightService } from './highlight.service';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [NzHighlightComponent],
-  exports: [NzHighlightComponent]
+  exports: [NzHighlightComponent],
 })
 export class NzHighlightModule {}
