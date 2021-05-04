70bc290202c05be94a5515c155d729b921d26860
Leo Dai <leo@jobadder.com>
2021-05-04 17:17:48 +1000

    export drawer-header component

--- projects/scales-library/src/public-api.ts
+++ projects/scales-library/src/public-api.ts
@@ -4,5 +4,6 @@
 
 export * from './lib/scales-library.module';
 export * from './lib/search/search.component';
+export * from './lib/drawer-header/drawer-header.component';
 
 export * from './lib/nz-zorro.module';
